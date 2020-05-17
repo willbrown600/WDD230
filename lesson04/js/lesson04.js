4e21611f2dfcf2680fabe553ff8e7fe86f98523b
willbrown600 <bro18085@byui.edu>
2020-05-17 14:49:37 +1000

    u

--- lesson04/js/lesson04.js
+++ lesson04/js/lesson04.js
@@ -5,6 +5,18 @@ function toggleMenu(){
 
 }
 
+
+const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//const MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+// get output location on document to append within list
+const output = document.getElementsByClassName("date");
+// get today's date and covert it to abbreviated day of the week text
+let tdate = new Date();
+let day = DAYS[tdate.getDay()];
+document.write("date").innerHTML = "Today is " + day; // output test
+
+
+
 function date(){
     document.getElementById("info").date;
 }
