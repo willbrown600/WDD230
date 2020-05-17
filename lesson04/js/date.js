ec9f68cbba0b157cc3715ed6bd6cbdebf0d61fc7
willbrown600 <bro18085@byui.edu>
2020-05-17 15:42:43 +1000

    r

--- lesson04/js/date.js
+++
@@ -1,8 +0,0 @@
-
-
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-
-document.getElementsByClassName('info').textContent = new Date().toLocaleDateString('en-US, options');
-
-const todaysdate = new Date();
-
