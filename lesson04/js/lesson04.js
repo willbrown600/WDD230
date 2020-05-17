b3dafc2cd1d6ba9aa068b44fd4cff520a7df6a83
willbrown600 <bro18085@byui.edu>
2020-05-17 15:06:06 +1000

    u

--- lesson04/js/lesson04.js
+++ lesson04/js/lesson04.js
@@ -1,3 +1,9 @@
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+
+document.getElementById('date').textContent = new Date().toLocaleDateString('en-US, options');
+
+const todaysdate = new Date();
+
 
 function toggleMenu(){ 
     console.log("It worked");   
@@ -6,17 +12,3 @@ function toggleMenu(){
 }
 
 
-const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-//const MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-// get output location on document to append within list
-const output = document.getElementsByClassName("date");
-// get today's date and covert it to abbreviated day of the week text
-let tdate = new Date();
-let day = DAYS[tdate.getDay()];
-document.write("date").innerHTML = "Today is " + day; // output test
-
-
-
-function date(){
-    document.getElementById("info").date;
-}
