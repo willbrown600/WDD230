6911c294b5a9c3612601d28d599a5a631f530e42
willbrown600 <bro18085@byui.edu>
2020-05-21 10:15:04 +1000

    u

--- lesson05/js/lesson05.js
+++ lesson05/js/lesson05.js
@@ -4,4 +4,12 @@ function toggleMenu(){
 
 }
 
+function date(){
+
+    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    document.getElementsById('date').textContent = new Date().toLocaleDateString('en-US, options');
+    const todaysdate = new Date();
+
+}
+
 
