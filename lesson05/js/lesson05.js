7f3f50e500bec263db3ca21a1245d9883c47e038
willbrown600 <bro18085@byui.edu>
2020-05-21 10:37:36 +1000

    u

--- lesson05/js/lesson05.js
+++ lesson05/js/lesson05.js
@@ -4,11 +4,18 @@ function toggleMenu(){
 
 }
 
+/*function onDocumentLoad(){
+    date();
+}*/
+
+
 function date(){
 
-    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    /*const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
     document.getElementsById('date').textContent = new Date().toLocaleDateString('en-US, options');
-    const todaysdate = new Date();
+    const todaysdate = new Date();*/
+
+    document.getElementById("date").innerHTML = "test";
 
 }
 
