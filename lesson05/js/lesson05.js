be29432c300560a1a87103498af12a41ce55b84d
willbrown600 <bro18085@byui.edu>
2020-05-24 15:47:21 +1000

    u

--- lesson05/js/lesson05.js
+++ lesson05/js/lesson05.js
@@ -12,11 +12,33 @@ function toggleMenu(){
 function date(){
 
     const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-    document.getElementsById('date').textContent = new Date().toLocaleDateString('en-US, options');
+    document.getElementsById('date').innerHTML = new Date().toLocaleDateString('en-US, options');
     const todaysdate = new Date();
 
     document.getElementById("date").innerHTML = "test";
 
 }
+
+
+var d = new Date();
+var n = d.getFullYear();
+
+function Index(){
+document.getElementById("copy").innerHTML = n;
+document.getElementById("date").innerHTML = "Last Updated: " + document.lastModified;
+
+}
+
+const date = new Date();
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const dayName = days[date.getDay()];
+
+const months = 
+
+if(dayName == 'Friday') {
+   document.getElementById("alert").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+   document.getElementById("alert").classList.add("show");
+}
+
 
 
