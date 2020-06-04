705cfdffabac505c7c5ab1c337628bb874d95df4
willbrown600 <bro18085@byui.edu>
2020-06-04 19:09:09 +1000

    update

---
+++ lesson07/js/lesson06.js
@@ -0,0 +1,25 @@
+function toggleMenu(){ 
+    console.log("It worked");   
+    document.getElementById("primaryNav").classList.toggle("hide");    
+
+}
+
+function date(){
+    const todaysdate = new Date();
+    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    document.getElementsById('modified').innerHTML = new Date().toLocaleDateString('en-US, options');
+    
+
+}
+
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const dayName = days[d.getDay()];
+
+if(dayName == 'Friday') {
+   document.getElementById("alert").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+   document.getElementById("alert").classList.add("show");
+}
+
+
+
