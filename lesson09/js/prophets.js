0d83623d0ce2bca85cba2682c48d4f215960489b
willbrown600 <bro18085@byui.edu>
2020-06-18 10:40:06 +1000

    u

--- lesson09/js/prophets.js
+++ lesson09/js/prophets.js
@@ -3,24 +3,36 @@ const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-
 fetch(requestURL)
   .then(function (response) {
     return response.json();
-  })
+})
   .then(function (jsonObject) {
     console.table(jsonObject);
-    for (let i = 0; i < prophets.length; i++ ) {
-    // temporary checking for valid response and data parsing
-        const prophets = jsonObject['prophets'];}
+    
+    const prophets = jsonObject['prophets'];
 
-  });
+    //loop through prophets arraylist creating elements for each prophet.
+    for(let i = 0; i < prophets.length; i++ ) {
+      let card = document.createElement('section');
+      let title = document.createElement('h2');
+      let birthdate = document.createElement('p');
+      let bornAt = document.createElement('p');
+      let photo = document.createElement('img');
 
+      //Add the text and images to the cards.
+      birthdate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+      bornAt.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+      photo.setAttribute('src', prophets[i].imageurl);
+      photo.setAttribute('alt', prophets[i].name + prophets[i].lastname + " - " + prophets[i].order);
+      title.textContent = prophets[i].name + ' ' + prophets[i].lastname;
 
-/*
-let card = document.createElement('section');
-let h2 = document.createElement('h2');
+      //add elements to arrayList
+      card.appendChild(title);
+      card.appendChild(birthdate);
+      card.appendChild(bornAt);
+      card.appendChild(photo);
 
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+      document.querySelector('div.cards').appendChild(card);
+  }
+});
 
-card.appendChild(h2);
+  
 
-document.querySelector('div.cards').appendChild(card);
-
-*/
