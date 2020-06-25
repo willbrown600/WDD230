04810583b63e669707149a506cf7b194ce54cef6
willbrown600 <bro18085@byui.edu>
2020-06-25 21:38:33 +1000

    update

--- lesson06/js/lesson06.js
+++ lesson06/js/lesson06.js
@@ -1,25 +1,21 @@
-function toggleMenu(){ 
-    console.log("It worked");   
-    document.getElementById("primaryNav").classList.toggle("hide");    
-
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
 }
 
-function date(){
-    const todaysdate = new Date();
-    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-    document.getElementsById('modified').innerHTML = new Date().toLocaleDateString('en-US, options');
-    
+function date() {
+    var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var d = new Date();
+    var weekday = d.getDay(); 
+    var dayOfMonth = d.getDate();
+    var month = d.getMonth();
+    var year = d.getFullYear();
 
+    var fullDate = weekdays[weekday] + ', ' + dayOfMonth + " " + months[month] + " " + year;
+    document.getElementById("modified").innerHTML = fullDate;
+
+    if (weekday === 5) {
+        document.getElementById("announcements").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+        document.getElementById("announcements").style.display = "block";
+    } 
 }
-
-
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const dayName = days[d.getDay()];
-
-if(dayName == 'Friday') {
-   document.getElementById("alert").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-   document.getElementById("alert").classList.add("show");
-}
-
-
-
