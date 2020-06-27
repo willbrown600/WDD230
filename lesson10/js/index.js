c38e9648e6ef302942bf1a6c7368613a386cf55b
willbrown600 <bro18085@byui.edu>
2020-06-28 06:40:14 +1000

    u

--- lesson10/js/index.js
+++ lesson10/js/index.js
@@ -1,22 +1,29 @@
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=e5b6156b8b34ff79f7a93c6716b2017b&units=imperial";
 
-
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
 
-    const
+    const currentTemp = document.querySelector('p1');
+    const highTemp = document.querySelector('p2');
+    const humidity = document.querySelector('p4');
+    const windchill = document.querySelector('p3');
+    const windspeed = document.querySelector('p5')
 
-    const currentTemp = document.querySelector('#current-temp');
     const icon = document.querySelector('img');
 
-    currentTemp.textContent = jsObject.main.temp;
+    currentTemp.textContent = jsObject.weather[0].description;
+    highTemp.textContent = jsObject.main.temp_max;
+    humidity.textContent = jsObject.main.humidity;
+    windchill.textContent = jsObject.main.feels_like;
+    windspeed.textContent = jsObject.wind.speed;
 
-    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+
+
+    
     const desc = jsObject.weather[0].description;
 
     
-    icon.setAttribute('src', imagesrc);
-    icon.setAttribute('alt', desc);
+    
   });
